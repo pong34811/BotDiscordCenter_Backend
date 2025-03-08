edc35fb3c51792cd58c2b49e76d8a3ea6e39d622
kayna-omen <pong34811@gmail.com>
2025-03-08 10:45:23 +0700

    update project django

--- botdiscordcenter_backend/wsgi.py
+++ botdiscordcenter_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for botdiscordcenter_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'botdiscordcenter_backend.settings')
 
 application = get_wsgi_application()
 
